ff0a770121f28b9a624c1021147531cb545bcaec
varioustoxins <garyt.and.sarahb@gmail.com>
2021-09-29 08:47:13 +0100

    wget to mirror directory

--- scripts/get_urls.py
+++ scripts/get_urls.py
@@ -1,7 +1,11 @@
 import sys
+from urllib.parse import urlparse
+import os
 
 if __name__ == '__main__':
     with open(sys.argv[1]) as fh:
         next(fh)
         for line in fh:
-            print(line.split()[0])
+            url = line.split()[0]
+            file_name = os.path.basename(urlparse(url).path)
+            print(url, file_name)
