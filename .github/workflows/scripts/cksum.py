d14f7a7282bfcf923241dbe51e187c733116bfa2
varioustoxins <garyt.and.sarahb@gmail.com>
2021-10-02 11:20:00 +0100

    add checksumming

---
+++ .github/workflows/scripts/cksum.py
@@ -0,0 +1,44 @@
+import os
+import sys
+from pathlib import Path
+import pycksum
+
+
+
+from icecream import ic
+from url_to_filename import url_to_filename
+
+EXIT_ERROR = 1
+
+if __name__ == '__main__':
+    with open(sys.argv[1]) as fh:
+
+        print(next(fh).strip(), file=sys.stderr)
+
+        for line in fh:
+            line = line.strip()
+            url,  bytes, chksum = line.split()
+            file_name = url_to_filename(url)
+
+
+            print(line, file=sys.stderr)
+
+            path = Path('MIRROR') / file_name
+            if bytes != '.':
+                bytes = int(bytes)
+                bytes_on_file_system = os.path.getsize(path)
+                if bytes !=  bytes_on_file_system:
+                    print(f'Error: size of file from url {url} does not match (expected {bytes} bytes got {bytes_on_file_system} bytes)', file=sys.stderr)
+                    print('Exiting..', file=sys.stderr)
+                    sys.exit(EXIT_ERROR)
+
+            if chksum != '.':
+                chksum =  int(chksum)
+                chksum_on_file_system = pycksum.cksum(open(path, 'rb'))
+                if bytes !=  bytes_on_file_system:
+                    print(f'Error: chksum of file from url {url} does not match (expected {chksum}  got {chksum_on_file_system})', file=sys.stderr)
+                    print('Exiting..', file=sys.stderr)
+                    sys.exit(EXIT_ERROR)
+
+        print(file=sys.stderr)
+        print('All OK', file =sys.stderr)
